ea51265839e7ee30989dd9d9721d3d1a42b6be59
Jacek <52275170+jpolskicom@users.noreply.github.com>
2021-11-24 21:18:43 +0100

    SearchField component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,42 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SearchField />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SearchField from "./components/SearchField.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    SearchField,
+  },
+};
 </script>
 
 <style>
+*,
+*::after,
+*::before {
+  box-sizing: border-box;
+}
+html,
+body {
+  margin: 0;
+  padding: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgb(3, 59, 85);
+  font-size: 16px;
+  min-height: 100vh;
 }
 </style>
---
+++ src/components/SearchField.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="c-searchField" :class="{ 'c-searchField--ready': ready }">
+    <div class="c-searchField__row">Type some GitGub usernames...</div>
+    <div class="c-searchField__row">
+      <input
+        type="text"
+        v-model="inputValue"
+        v-on:keyup.enter="accept()"
+        class="c-searchField__input"
+      />
+      <button
+        v-on:click="accept()"
+        class="c-searchField__button"
+        :class="{ 'c-searchField__button--active': inputValue.length > 0 }"
+      >
+        Zatwierdź
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      ready: false,
+    };
+  },
+  computed: {
+    inputValue: {
+      get() {
+        return "test";
+      },
+      set() {
+        return "test";
+      },
+    },
+  },
+  methods: {
+    accept() {},
+  },
+  mounted() {
+    // for start animation purposes only
+    setTimeout(() => (this.ready = true), 10);
+  },
+};
+</script>
+
+<style lang="scss">
+.c-searchField {
+  width: 0px;
+  overflow-x: hidden;
+  transition: width 2s;
+  &--ready {
+    width: 100%;
+  }
+  &__row {
+    width: calc(100% - 2rem);
+    margin: 1rem auto;
+    @media (min-width: 780px) {
+      display: flex;
+      justify-content: center;
+      width: 480px;
+    }
+  }
+  &__input {
+    -webkit-appearance: none;
+    width: 100%;
+    padding: 1rem 2rem;
+    font-size: 1rem;
+    border: 0;
+    outline: none;
+    @media (min-width: 780px) {
+      border-top-left-radius: 1rem;
+      border-bottom-left-radius: 1rem;
+    }
+  }
+  &__button {
+    padding: 1rem 2rem;
+    background-color: rgb(133, 167, 39);
+    font-size: 1rem;
+    border: 0;
+    @media (min-width: 780px) {
+      border-top-right-radius: 1rem;
+      border-bottom-right-radius: 1rem;
+    }
+
+    &--active {
+      background-color: rgb(175, 221, 50);
+    }
+  }
+}
+</style>
